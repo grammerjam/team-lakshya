7b4cd8ba46606b90d463ccfdceb5f0c498b70788
vaishali <vaishalirdj14@gmail.com>
2024-06-20 13:25:46 +0530

    user and movie schema added, new schema models added- shows, subscription, likes, comments, reviews, watchlist

--- P1/Project Brief/entertainment-web-app/server/src/models/movie.model.js
+++ P1/Project Brief/entertainment-web-app/server/src/models/movie.model.js
@@ -1,7 +1,73 @@
 import mongoose , { Schema } from "mongoose" ;
 
 const movieSchema = new Schema({
-
+    title : {
+        type : String ,
+        required : true ,
+        index : true ,
+    },
+    description : {
+        type : String , 
+        required : true ,
+    } ,
+    genre : {
+        type : String ,
+        enum : [ "Horror" , "Sports" , "Medical" , "Crime" , "Thriller" , "Pyschological" ] 
+    } ,
+    director : {
+        type : String ,
+        required : true ,
+    } ,
+    producer : {
+        type : String , 
+        required : true ,
+    } ,
+    studio : {
+        type : String ,
+        required : true ,
+    },
+    artists : {
+        type : [ String ] ,
+    } ,
+    videoFile : {
+        type : String ,
+        required : true ,
+    } ,
+    rated : {
+        type : String ,
+        enum : [ "U" , "A" , "R" ] ,
+    } ,
+    reviews : [
+        {
+            type : Schema.Types.ObjectId ,
+            ref : "Review"
+        }
+    ] ,
+    year : {
+        type : Number ,
+        required : true ,
+    } ,
+    audioLanguages : {
+        type : String ,
+        enum : [ "English" , "Hindi" , "Bengali" , "Maithli" ] 
+    } ,
+    subtitles : {
+        type : String , 
+        enum : [ "English" , "Hindi" , "French" , "Tamil" ] 
+    } ,
+    contentAdvisory : {
+        type : [ String ] 
+    } ,
+    likes : {
+        type : Schema.Types.ObjectId ,
+        ref : "Like"
+    } ,
+    comments : [
+        {
+            type : Schema.Types.ObjectId ,
+            ref : "Comment"
+        }
+    ]
 } , { timestamps : true } ) ;
 
 export const Movie = mongoose.model('Movie' , movieSchema ) ; 
--- P1/Project Brief/entertainment-web-app/server/src/models/user.model.js
+++ P1/Project Brief/entertainment-web-app/server/src/models/user.model.js
@@ -1,7 +1,52 @@
-import mongoose , {Schema} from "mongoose" ;
+import mongoose, { Schema } from 'mongoose';
 
-const UserSchema = new Schema({
-    
-} , {timestamps : true} ) ; 
+const mediaSchema = new Schema({
+	show: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Shows',
+	},
+	movie: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Movie',
+	},
+});
+const UserSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			index: true,
+			lowercase: true,
+		},
+		fullName: {
+			type: String,
+		},
+		password: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		watchHistory: {
+			type: [mediaSchema],
+		},
+		subscriptions: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Subscription',
+			},
+		],
+		watchList: {
+			type: [mediaSchema],
+		},
+		avatar: {
+			type: String,
+		},
+		refreshTokens: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
 
-export const User = mongoose.model('User' , UserSchema) ;
+export const User = mongoose.model('User', UserSchema);
