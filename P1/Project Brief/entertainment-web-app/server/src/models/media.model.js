9947a3cdafb9cee0dec72ce314a9531167aedeb7
vaishali <vaishalirdj14@gmail.com>
2024-06-24 17:40:36 +0530

    media model modified as per data.json

--- P1/Project Brief/entertainment-web-app/server/src/models/media.model.js
+++ P1/Project Brief/entertainment-web-app/server/src/models/media.model.js
@@ -1,6 +1,10 @@
 import mongoose, { Schema } from 'mongoose';
 
 const mediaSchema = new Schema({
+	category : {
+		type : String ,
+		required : true ,
+	},
 	show: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Shows',
@@ -9,6 +13,14 @@ const mediaSchema = new Schema({
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Movie',
 	},
+	isTrending : {
+		type : Boolean , 
+		required : true ,
+	} ,
+	isBookmarked : {
+		type : Boolean ,
+		required : true ,
+	}
 });
 
 export const Media = mongoose.model("Media" , mediaSchema) ;
