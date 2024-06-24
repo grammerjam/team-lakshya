8252e85d1f294e4467e6987cc6a98c4538becae2
vaishali <vaishalirdj14@gmail.com>
2024-06-24 17:19:41 +0530

    modified

--- P1/Project Brief/entertainment-web-app/server/src/controllers/user.controller.js
+++ P1/Project Brief/entertainment-web-app/server/src/controllers/user.controller.js
@@ -1,7 +1,14 @@
 import User from '../models/user.model.js';
 
-const registerUser = () => {
-    // registration logic 
+const registerUser = async ( req , res ) => {
+    // registration logic  
+    try {
+        const { username , email , password } = req.body ;
+        
+    } catch( error ) {
+
+    }
+
 };
 
 export { registerUser };
